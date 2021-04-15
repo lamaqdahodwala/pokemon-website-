06c39d927359cbaf44833457743177737827c468
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-15 12:44:27 -0400

    woring

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('id', views.byId, name='byid')
+    path('id', views.byId, name='byid'),
+    path('name', views.byName, name='byname'),
+    path('pokemon/<int:id>', views.pokemon, name='pokemon')
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, redirect
 import requests
+import django
 import json
 
 # Create your views here.
-def index(req):
+def index(req:django.core.handlers.wsgi.WSGIRequest):
     return render(req, 'index.html')
 
 
-def byId(req):
+def byId(req:django.core.handlers.wsgi.WSGIRequest):
     if  req.method == 'POST':
         post = req.POST or None
         if post:
             poke_id = post['id-input']
-            text = requests.get('https://pokeapi.co/api/v2/pokemon/{}'.format(poke_id)).text
-            data = json.loads(text)
-            with open('test.json', 'w') as f:
-                json.dump(data, f, indent='    ')
+            return redirect(req, 'pokemon')
         else:
             return redirect('index')
     return render(req, 'byid.html')
+
+def byName(req:django.core.handlers.wsgi.WSGIRequest):
+    if req.method == 'POST':
+        post = req.POST or None
+        if post:
+            poke_id = post['name-input']
+    return render(req, 'byname.html')
+
+def pokemon(req, id):
+    data = requests.get('https://pokeapi.co/api/v2/pokemon/{}'.format(id))
+    data = json.loads(data.text)
+    abilities = data['abilities']
+    abilities = [i['ability']['name'].title() for i in abilities]
+    basexp = data['base_experience']
+    name = data['forms'][0]['name'].title()
+    return render(req, 'pokemon.html', {'abilities': abilities, 'basexp': basexp, 'name': name})
